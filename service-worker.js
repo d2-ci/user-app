8d0e97740d697394601dde8e46b961761df1971e
GitHub <noreply@github.com>
2020-09-09 10:56:57 +0000

    52ec974 chore(deps): bump elliptic from 6.4.1 to 6.5.3

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2b1fe33a0dcc8e8c305a049dfd9fedd5.js"
+  "./precache-manifest.5446ad4e9d4035883eaa61d7223a419f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
