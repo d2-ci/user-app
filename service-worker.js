b45431891627e41c4f2e6ea44550b3ef6b0cab90
GitHub <noreply@github.com>
2020-12-01 12:01:14 +0000

    08e4dbe ci: migrate to github actions (#606)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d8e643c03fb78e61ebd8cc486f7a8629.js"
+  "./precache-manifest.5df0f856a6b046040cfd88b546c4d7c2.js"
 );
 
 self.addEventListener('message', (event) => {
